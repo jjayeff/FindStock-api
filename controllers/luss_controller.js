4bef00be655eafeb55f3cd4e66a5a629e66f9951
jayjeff <ekkawit.lertkrai@gmail.com>
2019-07-13 15:22:40 +0700

    Fixed bugs post create luss product

--- controllers/luss_controller.js
+++ controllers/luss_controller.js
@@ -26,18 +26,20 @@ module.exports = {
     var sql =
       `INSERT INTO luss_products (name, price, color, size, type, img_0, img_1, img_2, img_3, img_4, img_5, admin) ` +
       `VALUES ` +
-      `('${req.body.name}', ` +
-      `'${req.body.price}', ` +
-      `'${req.body.color}', ` +
-      `'${req.body.size}', ` +
-      `'${req.body.type}', ` +
-      `'${req.body.img_0}', ` +
-      `'${req.body.img_1}', ` +
-      `'${req.body.img_2}', ` +
-      `'${req.body.img_3}', ` +
-      `'${req.body.img_4}', ` +
-      `'${req.body.img_5}', ` +
-      `'${req.body.admin}') `;
+      `(${req.body.name ? `'${req.body.name}'` : 'null'}, ` +
+      `${req.body.price ? `'${req.body.price}'` : 'null'}, ` +
+      `${req.body.color ? `'${req.body.color}'` : 'null'}, ` +
+      `${req.body.size ? `'${req.body.size}'` : 'null'}, ` +
+      `${req.body.type ? `'${req.body.type}'` : 'null'}, ` +
+      `${req.body.img_0 ? `'${req.body.img_0}'` : 'null'}, ` +
+      `${req.body.img_1 ? `'${req.body.img_1}'` : 'null'}, ` +
+      `${req.body.img_2 ? `'${req.body.img_2}'` : 'null'}, ` +
+      `${req.body.img_3 ? `'${req.body.img_3}'` : 'null'}, ` +
+      `${req.body.img_4 ? `'${req.body.img_4}'` : 'null'}, ` +
+      `${req.body.img_5 ? `'${req.body.img_5}'` : 'null'}, ` +
+      `${
+        req.body.admin || req.body.admin === 0 ? `'${req.body.admin}'` : 'null'
+      }) `;
     try {
       result = await pool.query(sql);
       res.send({
